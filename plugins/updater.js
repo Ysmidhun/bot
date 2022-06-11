d242ddd7ad1e8eae69c2a53a327004dd562dbd03
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-06-11 21:39:31 +0530

    Updater UI improvements

--- plugins/updater.js
+++ plugins/updater.js
@@ -12,39 +12,63 @@ const exec = require('child_process').exec;
 const Heroku = require('heroku-client');
 const { PassThrough } = require('stream');
 const heroku = new Heroku({ token: Config.HEROKU.API_KEY })
-
-Module({pattern: 'update check$',use: 'owner', fromMe: true, dontAddCommandList: true, desc: "Checks bot updates"}, (async (message, match) => {
-    await git.fetch();
+Module({
+    pattern: 'update',
+    fromMe: true,
+    desc: "Updates bot",
+    use: 'owner'
+}, (async (message, match) => {
+     await git.fetch();
     var commits = await git.log(['main' + '..origin/' + 'main']);
+    var mss = '';
     if (commits.total === 0) {
-        await message.client.sendMessage(message.jid, { text:"Bot up to date"})
-
+    mss = "*Bot up to date*";
+    var buttons = [{
+        urlButton: {
+            displayText: 'WIKI',
+            url: 'https://github.com/souravkl11/raganork-md/wiki'
+        }
+    }];
     } else {
-        var degisiklikler = "Updates are available\n";
+        var changelog = "Updates are available\n";
         commits['all'].map(
             (commit) => {
-                degisiklikler += '(' + commit.date.substring(0, 10) + ') : *' + commit.message.replace('Update','Fixed').replace('.js','') + '*\n';
+                changelog += `${commit.message} ${'```'}[${commit.date.substring(0, 10)}]${'```'} *\n`
             }
         );
-        
-        await message.client.sendMessage(message.jid, { text:degisiklikler})
-
+        mss = changelog;
+        var buttons = [{
+        urlButton: {
+            displayText: 'WIKI',
+            url: 'https://github.com/souravkl11/raganork-md/wiki'
+        }
+    },
+    {
+        quickReplyButton: {
+            displayText: 'START UPDATE',
+            id: 'upd '+message.myjid
+        }
+    }];
     }
-}));
-
-Module({pattern: 'update start$',use: 'owner', fromMe: true,dontAddCommandList: true, desc: "Updates bot"}, (async (message, match) => {
-    await git.fetch();
+    await message.sendImageTemplate(await skbuffer("https://miro.medium.com/max/800/1*1y0JX_mOL_Xar63-iQAC0A.jpeg"),mss,"Feel free to update!",buttons);
+    }));
+Module({
+    on: 'button',
+    fromMe: true
+}, (async (message, match) => {
+    if (message.button && message.button.startsWith("upd") && message.button.includes(message.myjid)) {
+     await git.fetch();
     var commits = await git.log(['main' + '..origin/' + 'main']);
     if (commits.total === 0) {
         return await message.client.sendMessage(message.jid, { text:"_Bot up to date_"})
 
     } else {
-        await message.client.sendMessage(message.jid, { text:"_Build started_"})
+        await message.sendReply("_Build started ⏫_")
 
             try {
                 var app = await heroku.get('/apps/' + Config.HEROKU.APP_NAME)
             } catch {
-                await message.client.sendMessage(message.jid, { text:"Heroku information wrong!"})
+                await message.client.sendMessage(message.jid, { text:"*Heroku app name/api key wrong ❗*"})
 
                 await new Promise(r => setTimeout(r, 1000));
             }
@@ -57,12 +81,11 @@ Module({pattern: 'update start$',use: 'owner', fromMe: true,dontAddCommandList: 
             
             try {
                 await git.addRemote('heroku', git_url);
-            } catch { console.log('heroku remote ekli'); }
-            await git.push('heroku', 'main');
-
-            await message.client.sendMessage(message.jid, { text:"_Successfully updated_"})
-           await message.client.sendMessage(message.jid, { text:"_Restarting_"})
-            
-        
+            } catch {console.log('Deploy error catched. Retrying...')}
+            try { await git.push('heroku', 'main'); } catch(e){ 
+            if (e.message.includes("concurrent")) return await message.sendReply("Your account has reached in-parallel build limit! Please wait for the other app to finish its deploy ❗"); 
+            }
+            await message.sendReply("_Finished build! Restarting.._")
+         }
     }
-}));
+    }));
