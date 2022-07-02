472028dbf95a3b234382a0bfc74efcfa343f7593
Sourav K <souravkalathingals@gmail.com>
2022-07-02 17:59:37 +0530

    fix

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -56,7 +56,7 @@ Module({
         var res = await downloadGram(url[0])
         if (res == false) return await msg.sendReply("*Download failed*");
         for (var i in res) {
-        await msg.sendReply({url:res[i]}, res[i].includes("mp4")?'video':'image')
+        await msg.sendReply({url:res[i]}, res[i].includes("jpg")?'image':'video')
         };
     }
 }));
