55cb582404e57cf4acc84edfc28ebb1fbc37eb44
Sourav K <souravkalathingals@gmail.com>
2022-07-07 13:57:14 +0530

    added black, date calculator

--- plugins/media_tools.js
+++ plugins/media_tools.js
@@ -11,6 +11,7 @@ const {
   const {
       saveMessage
   } = require('./misc/saveMessage');
+  const {skbuffer} = require('raganork-bot');
   const {
       trim,
       AVmix,
@@ -59,6 +60,30 @@ const {
           });
       }
   });
+  Module({
+    pattern: "black",
+    fromMe: fromMe,
+    desc: "Audio to black video",
+    use: 'edit'
+}, async (message, match) => {
+    if (!fs.existsSync("./temp/avmix")) {
+        fs.mkdirSync("./temp/avmix")
+    }
+    let files = fs.readdirSync("./temp/avmix/")
+    if (!message.reply_message || !message.reply_message.audio) return await message.sendMessage("_Need audio!_");
+    var savedFile = await saveMessage(message.reply_message);
+        await fs.writeFileSync('./temp/avmix/audio.mp3', fs.readFileSync(savedFile));
+        await fs.writeFileSync('./temp/avmix/video.jpg', await skbuffer("https://i.imgur.com/4pybvW1.jpeg"));
+        await message.sendReply("_Processing..._")
+        
+    AVmix('./temp/avmix/video.jpg', './temp/avmix/audio.mp3', './temp/avmix/mixed.mp4', async function(video) {
+        await message.sendMessage(video, 'video');
+        await fs.unlinkSync('./temp/avmix/video.jpg');
+        await fs.unlinkSync('./temp/avmix/audio.mp3');
+        await fs.unlinkSync('./temp/avmix/mixed.mp4');
+        return;
+    });
+});
   Module({
       pattern: "avmix",
       fromMe: fromMe,
