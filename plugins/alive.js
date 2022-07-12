24f3b72357ce3116d5c4652c94025f8b86308a54
YSMIDHUN <109042588+YSMIDHUN@users.noreply.github.com>
2022-07-12 23:57:37 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -34,7 +34,7 @@ Module({
   var buttons = [{
     urlButton: {
         displayText: 'Github',
-        url: 'https://github.com/souravkl11/raganork'
+        url: 'https://github.com/YSMIDHUN/APARNA_V3_MD'
     }
 }, {
   urlButton: {
