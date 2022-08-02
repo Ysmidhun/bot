f674047eee3a2255f5729716c85072d31a8bc370
YSMIDHUN <109042588+YSMIDHUN@users.noreply.github.com>
2022-08-02 12:08:49 +0530

    Update lang.js

--- plugins/misc/lang.js
+++ plugins/misc/lang.js
@@ -1,6 +1,6 @@
- const {LANGUAGE,VERSION} = require('../../config');
+const {LANGUAGE,VERSION} = require('../../config');
 const {existsSync,readFileSync} = require('fs');
 var json = existsSync(__dirname+'/lang/' + LANGUAGE + '.json') ? JSON.parse(readFileSync(__dirname+'/lang/' + LANGUAGE + '.json')) : JSON.parse(readFileSync(__dirname+'/lang/english.json'));
-console.log("raganork-md "+VERSION)
+console.log("APARNA_V3_MD "+VERSION)
 function getString(file) { return json['STRINGS'][file]; }
 module.exports = {language: json, getString: getString }
