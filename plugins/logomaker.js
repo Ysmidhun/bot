f38a273f9725a9423f5e1fdb4c1d9c4f3347d9f3
YSMIDHUN <109042588+YSMIDHUN@users.noreply.github.com>
2022-08-02 12:44:56 +0530

    Update logomaker.js

--- plugins/logomaker.js
+++ plugins/logomaker.js
@@ -1,7 +1,7 @@
 const {Module} = require('../main')
 const {aadhar,blur} = require('./misc/misc')
 const {MODE} = require('../config')
-const {skbuffer} = require('raganork-bot');
+const {skbuffer} = require('APARNA_V3_MD');
 var x = MODE == 'public'?false:true
 var list = '```'+`Logo Maker List
 Usage: .logo 14 Text
