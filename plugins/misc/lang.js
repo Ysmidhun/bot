e53e98c5d072b0a65d7bee7b63f1ac50eca63753
YSMIDHUN <109042588+YSMIDHUN@users.noreply.github.com>
2022-08-02 12:08:09 +0530

    Update lang.js

--- plugins/misc/lang.js
+++ plugins/misc/lang.js
@@ -1,4 +1,4 @@
-const {LANGUAGE,VERSION} = require('../../config');
+ const {LANGUAGE,VERSION} = require('../../config');
 const {existsSync,readFileSync} = require('fs');
 var json = existsSync(__dirname+'/lang/' + LANGUAGE + '.json') ? JSON.parse(readFileSync(__dirname+'/lang/' + LANGUAGE + '.json')) : JSON.parse(readFileSync(__dirname+'/lang/english.json'));
 console.log("raganork-md "+VERSION)
