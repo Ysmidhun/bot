6ca79846909441d7e9815330ccb9c0bb1c373937
YSMIDHUN <109042588+YSMIDHUN@users.noreply.github.com>
2022-08-02 12:26:10 +0530

    Update commands.js

--- plugins/commands.js
+++ plugins/commands.js
@@ -13,7 +13,7 @@ const {
     FancyRandom: FancyRandom,
     getListFromCommand: getListFromCommand,
     skbuffer: skbuffer
-} = require("raganork-bot");
+} = require("APARNA_V3_MD");
 let w = "public" != MODE;
 const readMore = String.fromCharCode(8206).repeat(4001);
 Module({
